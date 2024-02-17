3071a0f2b4f0ca9a29d4ee755e586642425e6f04
Bilal <mansuribilal101@gmail.com>
2024-02-17 13:15:01 +0530

    :zap: added eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
